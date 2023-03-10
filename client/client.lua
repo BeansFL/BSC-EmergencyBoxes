1a9851fce268200a17e04fa5aca02529954a05f6
cmd <felixbusiness332000@gmail.com>
2023-03-10 12:20:04 +0100

    disableplayerfiring

--- client/client.lua
+++ client/client.lua
@@ -42,26 +42,46 @@ CreateThread(function()
   for i, spawnPoint in ipairs(Config.spawnPoints) do 
     spawnCallbox(spawnPoint.x, spawnPoint.y, spawnPoint.z, spawnPoint.heading)
     TaskTurnPedToFaceCoord(playerPed, spawnPoint.x, spawnPoint.y, spawnPoint.z, 0) 
-  end 
-  while true do 
+  end
+
+  while true do
+    
     Wait(0)
     local playerPed = PlayerPedId()
     local playerCoords = GetEntityCoords(playerPed)
     ESX.Streaming.RequestAnimDict("anim@cellphone@in_car@ps", cb) 
     ESX.Streaming.RequestAnimDict("anim@amb@trailer@touch_screen@", cb) 
-    -- Check if player is close to any callbox 
+
+
+    -- Check if player is close to any callbox
     for i, callboxProp in ipairs(callboxProps) do
       local callboxCoords = GetEntityCoords(callboxProp)  
       local distance = GetDistanceBetweenCoords(playerCoords, callboxCoords, true)
+
+
       if distance < 2.0 then
         -- Show help notification and check if player presses E 
         ESX.ShowHelpNotification('Press ~INPUT_CONTEXT~ to interact with the callbox')
         if IsControlJustPressed(0, 38) then 
-          DisablePlayerFiring(PlayerPedId(), true) 
+
+          local count = 1
+
+          while count > 500 do 
+            count = count + 1
+            DisablePlayerFiring(PlayerPedId(), true) 
+          end
+
+          -- DisablePlayerFiring(PlayerPedId(), true) 
           TaskPlayAnim(PlayerPedId(), 'anim@cellphone@in_car@ps', 'cellphone_call_listen_base', 1.0, 1.0, -1, 50, 0, false, false, false)
+
+
           Wait(5000) 
+
+
           ClearPedTasks(PlayerPedId()) 
           DisablePlayerFiring(PlayerPedId(), false)  
+
+
           print("Funktioniert") 
           -- TODO: drecks serverevents adden für dispatch 
         end
