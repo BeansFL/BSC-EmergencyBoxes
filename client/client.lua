3ba82dd65f9fff49743aeb506a4ac805ea8d9be6
BeansFL <106496863+BeansFL@users.noreply.github.com>
2023-03-10 11:56:20 +0100

    Add files via upload

---
+++ client/client.lua
@@ -0,0 +1,78 @@
+local callboxProps = {} -- Table to store all callbox props
+local spawnPoints = Config.spawnPoints
+-- Function to spawn a callbox at a specific coordinate
+function spawnCallbox(x, y, z, heading)
+  local modelHash = GetHashKey('prop_sign_road_callbox')
+  if not HasModelLoaded(modelHash) then
+    ESX.Streaming.RequestModel(modelHash, cb)
+    while not HasModelLoaded(modelHash) do
+      Wait(0)  
+    end
+  end
+  local callboxProp = CreateObject(modelHash, x, y, z, true, true, false)
+  SetEntityHeading(callboxProp, heading)
+  FreezeEntityPosition(callboxProp, true) 
+
+  -- Add blip for the spawn point
+  local blip = AddBlipForCoord(x, y, z)
+  SetBlipSprite(blip, 526) -- 2 is the blip sprite for a callbox
+  SetBlipDisplay(blip, 2) -- 2 is the display ID for blips on the radar and map
+  SetBlipScale(blip, 0.8) -- Set blip size 
+  SetBlipColour(blip, 47) -- Set blip color to blue
+  SetBlipAsShortRange(blip, true) -- Set blip to only show on close range
+  BeginTextCommandSetBlipName("STRING")
+  AddTextComponentString("SOS CALLBOX") -- set blip name
+  EndTextCommandSetBlipName(blip)
+ 
+  table.insert(callboxProps, callboxProp)
+end  
+
+-- Function to delete all callbox props 
+function deleteCallboxProps()
+  for i, callboxProp in ipairs(callboxProps) do
+    DeleteObject(callboxProp)
+  end
+  callboxProps = {}
+end
+
+-- Main loop 
+CreateThread(function()
+  for i, spawnPoint in ipairs(Config.spawnPoints) do 
+    spawnCallbox(spawnPoint.x, spawnPoint.y, spawnPoint.z, spawnPoint.heading)
+    TaskTurnPedToFaceCoord(playerPed, spawnPoint.x, spawnPoint.y, spawnPoint.z, 0) 
+  end 
+  while true do 
+    Wait(0)
+    local playerPed = PlayerPedId()
+    local playerCoords = GetEntityCoords(playerPed)
+    ESX.Streaming.RequestAnimDict("anim@cellphone@in_car@ps", cb) 
+    ESX.Streaming.RequestAnimDict("anim@amb@trailer@touch_screen@", cb) 
+    -- Check if player is close to any callbox 
+    for i, callboxProp in ipairs(callboxProps) do
+      local callboxCoords = GetEntityCoords(callboxProp)  
+      local distance = GetDistanceBetweenCoords(playerCoords, callboxCoords, true)
+      if distance < 2.0 then
+        -- Show help notification and check if player presses E 
+        ESX.ShowHelpNotification('Press ~INPUT_CONTEXT~ to interact with the callbox')
+        if IsControlJustPressed(0, 38) then
+          TaskPlayAnim(PlayerPedId(), 'anim@amb@trailer@touch_screen@"', 'idle_c', 1.0, 1.0, -1, 50, 0, false, false, false)
+          Wait(3000)
+          ClearPedTasks(PlayerPedId())  
+          TaskPlayAnim(PlayerPedId(), 'anim@cellphone@in_car@ps', 'cellphone_call_listen_base', 1.0, 1.0, -1, 50, 0, false, false, false)
+          Wait(5000)
+          ClearPedTasks(PlayerPedId()) 
+          print("Funktioniert")
+          -- TODO: drecks serverevents adden für dispatch 
+        end
+      end    
+    end  
+  end    
+end)
+
+-- Delete all callbox props when the script restarts
+AddEventHandler('onResourceStop', function(resource) 
+  if GetCurrentResourceName() == resource then
+    deleteCallboxProps() 
+  end 
+end)
+ 
---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+-- Configuration
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+-- Files Registration
+
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    '@es_extended/locale.lua',
+}
+
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+}
+
+server_scripts {
+    
+
+    'config.lua',
+    'server/server.lua',
+    
+}
+
